88f10e1c718adf2767f503486440abc0037e16d3
BalaGanesh I <balaig7@gmail.com>
2022-02-19 22:10:12 +0530

    master layouts added for admin and frontpage

---
+++ admin/content.php
@@ -0,0 +1,12 @@
+<?php
+include __DIR__."/layouts/menu.php";
+include __DIR__."/layouts/header.php";
+?>
+<div class="main-content">
+test
+</div>
+<?php
+include __DIR__."/layouts/footer.php";
+?>
+
+
---
+++ admin/layouts/footer.php
@@ -0,0 +1,23 @@
+</div><!--/#wrapper -->
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="assets/script/html5shiv.min.js"></script>
+		<script src="assets/script/respond.min.js"></script>
+	<![endif]-->
+	<!-- 
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/modernizr.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+	<script src="assets/js/nprogress.js"></script>
+	<script src="assets/js/sweetalert.min.js"></script>
+	<script src="assets/js/waves.min.js"></script>
+	<!-- Full Screen Plugin -->
+	<!-- <script src="assets/plugin/fullscreen/jquery.fullscreen-min.js"></script> -->
+
+	<script src="assets/js/main.min.js"></script>
+	<!-- <script src="assets/color-switcher/color-switcher.min.js"></script> -->
+</body>
+</html>
